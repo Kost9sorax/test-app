98e9fe6e1f25377d19afaf2c99d48b2ee1edac40
quake <39491148+Kost9sorax@users.noreply.github.com>
2022-03-24 18:23:54 +0500

    Add files via upload

---
+++ sql-alchemy.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.sql.schema import Column
+from config import Base, engine
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker()
+session = Session(bind=engine)
+
+
+class Device(Base):
+    __tablename__ = 'Devices'
+    id = Column(Integer(), primary_key=True)
+    dev_id = Column(String(200), nullable=False)
+    dev_type = Column(String(120), nullable=False)
+
+    def __init__(self, dev_id, dev_type):
+        self.dev_id = dev_id
+        self.dev_type = dev_type
+
+
+class Endpoint(Base):
+    __tablename__ = 'Endpoints'
+    id = Column(Integer, primary_key=True)
+    device_id = Column(Integer)
+    comment = Column(String)
+
+    def __repr__(self):
+        return f"Devices(id={self.id!r}, device_id={self.device_id!r}, comment={self.comment!r})"
+
+
+dev = Device('def', 'def')
+session.add(dev)
+session.commit()
+
