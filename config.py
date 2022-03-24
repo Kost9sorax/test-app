43f6fbc647649cda8ec22056deb9ac347dd3c0a4
quake <39491148+Kost9sorax@users.noreply.github.com>
2022-03-24 18:23:41 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
-host = '127.0.0.1'
-user = 'postgres'
-password = '1234'
-db_name = 'devices'
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+engine = create_engine("postgresql://postgres:1234@localhost/devices", echo=True)
+session_local = sessionmaker(autocommit=True, bind=engine)
+engine.connect()
+
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    except:
+        db.close()
