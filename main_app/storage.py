2130a668d3b54d34a1fb2b7aa8d9cde401d5d8a0
quake <39491148+Kost9sorax@users.noreply.github.com>
2022-03-26 20:56:37 +0500

    Update storage.py

--- main_app/storage.py
+++ main_app/storage.py
@@ -1,11 +1,12 @@
 import random
-from mac_generator import main
-from migrations.models import session, Device, Endpoint
-from config import engine
+from main_app.mac_generator import make_mac
+from migrartions.models import session, Device, Endpoint
+from main_app.config import engine
 from sqlalchemy import func
-from http import HTTPStatus
+
 types = ['emeter', 'zigbee', 'lora', 'gsm']
 conn = engine.connect()
+
 
 def append_elements():
     def get_type():
@@ -13,7 +14,7 @@ def append_elements():
         return dev_type
 
     def get_id():
-        dev_id = main()
+        dev_id = make_mac()
         return str(dev_id)
 
     for i in range(0, 10):
@@ -29,8 +30,7 @@ def append_elements():
     for i in range(5):
         session.add(Endpoint(ids[i], 'some comment'))
     session.commit()
-    
 
-devices_without_endpoint = session.query(func.count(Device.id), Device.dev_type).filter(
-    Device.id.notin_(session.query(Endpoint.device_id))).group_by(Device.dev_type).all()
-print(devices_without_endpoint)
+    devices_without_endpoint = session.query(func.count(Device.id), Device.dev_type).filter(
+        Device.id.notin_(session.query(Endpoint.device_id))).group_by(Device.dev_type).all()
+    print(devices_without_endpoint)
