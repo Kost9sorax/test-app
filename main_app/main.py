184a49c492f27d7fe5a830214a9d2420c70db236
quake <39491148+Kost9sorax@users.noreply.github.com>
2022-03-26 20:57:06 +0500

    Update main.py

--- main_app/main.py
+++ main_app/main.py
@@ -1,8 +1,28 @@
+from main_app.storage import *
 from fastapi import FastAPI
 import redis
+from fastapi import Response
 
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 app = FastAPI()
+
+
+@app.get('/{str_1}/{str_2}')
+def get_anagrams(str_1, str_2):
+    if is_anagrams(str_1, str_2):
+        if redis_client.get("counter"):
+            redis_client.set("counter", int(redis_client.get("counter")) + 1)
+        else:
+            redis_client.set("counter", 1)
+        return {"is anagrams": True, "counter": redis_client.get("counter")}
+    else:
+        return {"is anagrams": False}
+
+
+@app.get('/add')
+def add_elements():
+    append_elements()
+    return Response(status_code=201)
 
 
 def is_anagrams(str_1, str_2):
